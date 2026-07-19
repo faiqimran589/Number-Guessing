ec94bb7bb4be19a288ca8ec011cba9aa75f0ac40
faiqimran589 <faiqimran589@gmail.com>
2026-07-19 23:39:09 +0500

    Change logic

---
+++ number.py
@@ -0,0 +1,18 @@
+
+while True:
+ number=int((input('Enter the number between 1 and 100:')))
+ import random
+ d=random.randint(1,100)
+
+ if number > d :
+   print('Try lower number')
+ 
+ elif number < d:
+   print('Try greater number')
+  
+ elif number == d :
+   print('Congratulations')
+   break
+ else :
+    print('Number not recognized')
+    
